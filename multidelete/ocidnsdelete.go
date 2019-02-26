586785985ae872030805dc3a3fc12812da608785
sugimount <sugi.mount@gmail.com>
2019-02-26 05:53:13 +0000

    add multidelete (to Patch Function)

---
+++ multidelete/ocidnsdelete.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Sugi275/oci-env-configprovider/envprovider"
+	"github.com/oracle/oci-go-sdk/common"
+	"github.com/oracle/oci-go-sdk/dns"
+)
+
+func main() {
+	zn := "test.enc"
+	dn := "multitest.test.enc"
+
+	client, err := dns.NewDnsClientWithConfigurationProvider(envprovider.GetEnvConfigProvider())
+	if err != nil {
+		panic(err)
+	}
+
+	compartmentid, err := envprovider.GetCompartmentID()
+	if err != nil {
+		panic(err)
+	}
+
+	getRequest := dns.GetDomainRecordsRequest{
+		ZoneNameOrId:  common.String(zn),
+		Domain:        common.String(dn),
+		CompartmentId: common.String(compartmentid),
+	}
+
+	// Domainに設定されているすべてのレコードを取得
+	ctx := context.Background()
+	domainRecords, err := client.GetDomainRecords(ctx, getRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	var deletehash *string
+	for _, record := range domainRecords.RecordCollection.Items {
+		fmt.Print("Domain: ", *record.Domain, "  ")
+		fmt.Print("RecordHash: ", *record.RecordHash, "  ")
+		fmt.Println("Rdata: ", *record.Rdata)
+
+		if *record.Rdata == "\"deleteme\"" {
+			deletehash = record.RecordHash
+		}
+	}
+
+	// 一部のレコードを削除。rdataの中身がdeletemeのものを削除
+	fmt.Println("deletehash: ", *deletehash)
+	recordOperation := dns.RecordOperation{
+		RecordHash: deletehash,
+		Operation:  dns.RecordOperationOperationRemove,
+	}
+
+	patchRequest := dns.PatchDomainRecordsRequest{
+		ZoneNameOrId: common.String(zn),
+		Domain:       common.String(dn),
+		PatchDomainRecordsDetails: dns.PatchDomainRecordsDetails{
+			Items: []dns.RecordOperation{
+				recordOperation,
+			},
+		},
+		CompartmentId: common.String(compartmentid),
+	}
+
+	response, err := client.PatchDomainRecords(context.Background(), patchRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response)
+}
